3a5e1109b1998e9e759ba3e51909cc900fbf205d
Deloof Killian <killiandeloof@gmail.com>
2017-06-20 10:13:31 +0200

    pacages

---
+++ TSTP_PCL/TSTP_PCL.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("TSTP_PCL.Droid.MainApplication, TSTP_PCL.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md5080402b1dd0264a5811af1dbd7bddef5.MainApplication.class, md5080402b1dd0264a5811af1dbd7bddef5.MainApplication.__md_methods);
+		
+	}
+}
